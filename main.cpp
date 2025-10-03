282310979e57f421fadf47de0c3e4b2327e045c5
aschilaci <aschilaci.yt@gmail.com>
2025-10-03 21:33:29 +0200

    main.cpp hibak es logika javitva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // HIBA: NELEMENTS >>> N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' // HIBA: 'char' >>> "str", a C++ string dupla idezojelet hasznal, hianyzik a sor vege
-    for (int i = 0;) // HIBA: teljes for-fejlec (kezdet; feltetel; leptetes)
+    int *b = new int[N_ELEMENTS]; // HIBA: NELEMENTS >>> N_ELEMENTS
+    std::cout << "1-100 ertekek duplazasa" << std::endl; // HIBA: 'char' >>> "str", a C++ string dupla idezojelet hasznal, hianyzik a sor vege
+    for (int i = 0; i < N_ELEMENTS; i++) // HIBA: teljes for-fejlec (kezdet; feltetel; leptetes)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // HIBA: feltetel i < N_ELEMENTS (nem 0-kent ertelmezett i)
+    for (int i = 0; i < N_ELEMENTS; i++) // HIBA: feltetel i < N_ELEMENTS (nem 0-kent ertelmezett i)
     {
-        std::cout << "Ertek:" // HIBA: ertek kiirasa, sorvegi pontosvesszo es sortores
+        std::cout << "Ertek: " << b[i] << std::endl; // HIBA: ertek kiirasa, sorvegi pontosvesszo es sortores
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // HIBA: inicializalas 0-ra hianyzik
-    for (int i = 0; i < N_ELEMENTS, i++) // HIBA: vesszo helyett pontosvesszo a for-fejlecben
+    int atlag = 0; // HIBA: inicializalas 0-ra hianyzik
+    for (int i = 0; i < N_ELEMENTS; i++) // HIBA: vesszo helyett pontosvesszo a for-fejlecben
     {
-        atlag += b[i] // HIBA: sorvegi pontosvesszo
+        atlag += b[i]; // HIBA: sorvegi pontosvesszo
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b; // EXTRA: lefoglalt tomb felszabaditasa (memoriaszivargas elkerulese)
     return 0;
 }
